f76339658cb35cb82de01ca9cc08eeaea57211d1
raulgarcia22grados <raul@22grados.com>
2024-04-18 23:56:10 +0100

    First Commit

---
+++ src/components/react/CardStack.tsx
@@ -0,0 +1,50 @@
+import { CardStack } from '@/components/react/aceternityui/CardStack'
+
+export function CardStackDemo() {
+  return (
+    <div className="h-[40rem] flex items-center justify-center w-full">
+      <CardStack items={CARDS} />
+    </div>
+  );
+}
+
+const CARDS = [
+  {
+    id: 0,
+    name: "Manu Arora",
+    designation: "Senior Software Engineer",
+    content: (
+      <p>
+        These cards are amazing, I want to use them in my
+        project. Framer motion is a godsend ngl tbh fam 🙏
+      </p>
+    ),
+  },
+  {
+    id: 1,
+    name: "Elon Musk",
+    designation: "Senior Shitposter",
+    content: (
+      <p>
+        I dont like this Twitter thing,{" "}
+        deleting it right away because yolo. Instead, I
+        would like to call it X.com so that it can easily
+        be confused with adult sites.
+      </p>
+    ),
+  },
+  {
+    id: 2,
+    name: "Tyler Durden",
+    designation: "Manager Project Mayhem",
+    content: (
+      <p>
+        The first rule of
+        Fight Club is that you do not talk about fight
+        club. The second rule of
+        Fight club is that you DO NOT TALK about fight
+        club.
+      </p>
+    ),
+  },
+]; 
